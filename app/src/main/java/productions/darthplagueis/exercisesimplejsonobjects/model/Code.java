7abe083c420cbbb523f61b896413f2d62b9d3285
osibo00 <olegsiboglov@ac.c4q.nyc>
2017-12-17 14:51:22 -0500

    initial commit

---
+++ app/src/main/java/productions/darthplagueis/exercisesimplejsonobjects/model/Code.java
@@ -0,0 +1,76 @@
+package productions.darthplagueis.exercisesimplejsonobjects.model;
+
+import java.util.List;
+
+/**
+ * {
+ "colors": [
+ {
+ "color": "black",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,255,255,1],
+ "hex": "#000"
+ }
+ },
+ {
+ "color": "white",
+ "category": "value",
+ "code": {
+ "rgba": [0,0,0,1],
+ "hex": "#FFF"
+ }
+ },
+ {
+ "color": "red",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,0,0,1],
+ "hex": "#FF0"
+ }
+ },
+ {
+ "color": "blue",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [0,0,255,1],
+ "hex": "#00F"
+ }
+ },
+ {
+ "color": "yellow",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,255,0,1],
+ "hex": "#FF0"
+ }
+ },
+ {
+ "color": "green",
+ "category": "hue",
+ "type": "secondary",
+ "code": {
+ "rgba": [0,255,0,1],
+ "hex": "#0F0"
+ }
+ }
+ ]
+ }
+ */
+
+public class Code {
+    private List<Integer> rgba;
+    private String hex;
+
+    public List<Integer> getRgba() {
+        return rgba;
+    }
+
+    public String getHex() {
+        return hex;
+    }
+}
---
+++ app/src/main/java/productions/darthplagueis/exercisesimplejsonobjects/model/Color.java
@@ -0,0 +1,84 @@
+package productions.darthplagueis.exercisesimplejsonobjects.model;
+
+/**
+ * {
+ "colors": [
+ {
+ "color": "black",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,255,255,1],
+ "hex": "#000"
+ }
+ },
+ {
+ "color": "white",
+ "category": "value",
+ "code": {
+ "rgba": [0,0,0,1],
+ "hex": "#FFF"
+ }
+ },
+ {
+ "color": "red",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,0,0,1],
+ "hex": "#FF0"
+ }
+ },
+ {
+ "color": "blue",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [0,0,255,1],
+ "hex": "#00F"
+ }
+ },
+ {
+ "color": "yellow",
+ "category": "hue",
+ "type": "primary",
+ "code": {
+ "rgba": [255,255,0,1],
+ "hex": "#FF0"
+ }
+ },
+ {
+ "color": "green",
+ "category": "hue",
+ "type": "secondary",
+ "code": {
+ "rgba": [0,255,0,1],
+ "hex": "#0F0"
+ }
+ }
+ ]
+ }
+ */
+
+public class Color {
+    private String color;
+    private String category;
+    private String type;
+    private Code code;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Code getCode() {
+        return code;
+    }
+}
